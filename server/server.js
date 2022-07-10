065e53f04845d7c9a4e2409bafa7ed145a40edc9
Jérémy Renaut <jeremy.renaut1@gmail.com>
2022-07-10 02:12:33 +0200

    up

--- server/server.js
+++ server/server.js
@@ -3,5 +3,5 @@ import { Express } from "express";
 const app = express();
 
 app.listen("5000", () => {
-  console.log("Le serveur est démarré sur le port 5000");
+  console.log("Le serveur est démarré sur le port 5000 !");
 });
